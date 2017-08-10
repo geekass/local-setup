5e7d4d08ceca7504fd5b31d2dd298d50e88b5322
Richard Hewett <richard.alastair.hewett@gmail.com>
2017-08-10 13:44:16 +0100

    Update paths for linked scripts

--- Scripts/sources.sh
+++ Scripts/sources.sh
@@ -1,4 +1,4 @@
-source ~/Scripts/git_functions.sh
-source ~/Scripts/git-completion.sh
-source ~/Scripts/django_bash_completion.sh
-source ~/Scripts/bash_prompt.sh
+source ~/local-setup/Scripts/git_functions.sh
+source ~/local-setup/Scripts/git-completion.sh
+source ~/local-setup/Scripts/django_bash_completion.sh
+source ~/local-setup/Scripts/bash_prompt.sh
